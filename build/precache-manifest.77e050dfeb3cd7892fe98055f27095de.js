100041947772d65107dbb29c1b297fcacd2205f7
Bente Gber <benjamingreenberg@Benjamins-MacBook-Pro-2.local>
2019-04-12 16:04:09 -0400

    added text to loading screen

---
+++ build/precache-manifest.77e050dfeb3cd7892fe98055f27095de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "0647e70ec97f2c43f0dc",
+    "url": "/static/js/main.406bce97.chunk.js"
+  },
+  {
+    "revision": "e000fa0079b9d249b696",
+    "url": "/static/js/2.f993e5f6.chunk.js"
+  },
+  {
+    "revision": "0647e70ec97f2c43f0dc",
+    "url": "/static/css/main.5cf6027d.chunk.css"
+  },
+  {
+    "revision": "a4ff50d928bc157ec08b4dcbd913b185",
+    "url": "/index.html"
+  }
+];
